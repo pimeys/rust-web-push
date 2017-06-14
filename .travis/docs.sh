91aba63d3e9c644deae937526a974ae63cfbcd38
Julius de Bruijn <julius.debruijn@360dialog.com>
2017-06-14 15:37:35 +0200

    Try to get travis to push docs to github

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -6,7 +6,11 @@ shopt -s globstar
 
 cargo doc --no-deps
 
-git clone --branch gh-pages "https://$TOKEN@github.com/${TRAVIS_REPO_SLUG}.git" deploy_docs > /dev/null 2>&1
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+git clone --branch gh-pages $REPO deploy_docs
 cd deploy_docs
 
 git config user.name "Julius de Bruijn"
@@ -19,9 +23,19 @@ echo "<meta http-equiv=refresh content=0;url=web_push/index.html>" > ./master/in
 git add -A .
 git commit -m "rebuild pages at ${TRAVIS_COMMIT}"
 
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in github_travis_ecdsa.enc -out github_travis_ecdsa -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 echo
 echo "Pushing docs..."
-git push origin gh-pages
+git push $SSH_REPO gh-pages
 echo
 echo "Docs published."
 echo
