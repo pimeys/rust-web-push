21e749087d90f7a06584d6b0301085a1bae411b7
Julius de Bruijn <julius.debruijn@360dialog.com>
2017-06-15 09:42:45 +0000

    rebuild pages at 6aabe6738e0a77b8210c3db51a53534aa574197a

---
+++ v0.1.1/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;TimeoutError&lt;<a class=\"struct\" href=\"web_push/struct.WebPushResponse.html\" title=\"struct web_push::WebPushResponse\">WebPushResponse</a>&gt;&gt; for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://briansmith.org/rustdoc/ring/error/struct.Unspecified.html\" title=\"struct ring::error::Unspecified\">Unspecified</a>&gt; for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/native-tls/0.1.2/native_tls/struct.Error.html\" title=\"struct native_tls::Error\">Error</a>&gt; for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
