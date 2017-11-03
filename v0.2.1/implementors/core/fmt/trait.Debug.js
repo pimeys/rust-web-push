e62e3eb0c9794e14237edbe710a9f390409277f7
Julius de Bruijn <julius.debruijn@360dialog.com>
2017-11-03 14:48:42 +0000

    rebuild pages at b28cec96ff88f3c7097d9d55831b76d8ef072026

---
+++ v0.2.1/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"web_push/struct.WebPushResponse.html\" title=\"struct web_push::WebPushResponse\">WebPushResponse</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"web_push/struct.WebPushPayload.html\" title=\"struct web_push::WebPushPayload\">WebPushPayload</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"web_push/struct.WebPushMessage.html\" title=\"struct web_push::WebPushMessage\">WebPushMessage</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.2.1/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
