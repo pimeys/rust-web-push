1d4be940dbe3d553a0ef16bacfe02d8d102d68e7
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-04-04 19:20:23 +0000

    rebuild pages at f5f118c50bb83f3a31a8f4581ca76e8ba6ba8974

---
+++ master/implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for &amp;'a <a class=\"struct\" href=\"web_push/struct.VapidSignature.html\" title=\"struct web_push::VapidSignature\">VapidSignature</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
