6028d7815fbb7fc129d46f2c3debd6d834dc351a
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-05-31 14:22:24 +0000

    rebuild pages at 2a0d74124b82f979f3cbeaf4e5ae95d703e14652

--- master/implementors/std/error/trait.Error.js
+++ master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["web_push"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",];
+implementors["web_push"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",synthetic:false,types:["web_push::error::WebPushError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/tokio_service/trait.Service.js
+++ master/implementors/tokio_service/trait.Service.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["web_push"] = ["impl Service for <a class=\"struct\" href=\"web_push/struct.WebPushClient.html\" title=\"struct web_push::WebPushClient\">WebPushClient</a>",];
+implementors["web_push"] = [{text:"impl Service for <a class=\"struct\" href=\"web_push/struct.WebPushClient.html\" title=\"struct web_push::WebPushClient\">WebPushClient</a>",synthetic:false,types:["web_push::client::WebPushClient"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
