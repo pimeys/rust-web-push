4bfdf0792b6dbb322e7ce397c0e1adc6b2f483f7
Julius de Bruijn <julius.debruijn@360dialog.com>
2018-06-02 10:32:38 +0000

    rebuild pages at d1afe5fe4d818ab436ca706520166060748f3bd3

---
+++ v0.4.0/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",synthetic:false,types:["web_push::error::WebPushError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ v0.4.0/implementors/tokio_service/trait.Service.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web_push"] = [{text:"impl Service for <a class=\"struct\" href=\"web_push/struct.WebPushClient.html\" title=\"struct web_push::WebPushClient\">WebPushClient</a>",synthetic:false,types:["web_push::client::WebPushClient"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
