08bb1330e65317c0ce4dea4ba6aec0ea9035c86e
Julius de Bruijn <bruijn@prisma.io>
2019-12-17 11:20:29 +0100

    Enforce static lifetime for client's future
    - Helps when spawning

--- src/client.rs
+++ src/client.rs
@@ -8,6 +8,7 @@ use http::header::{RETRY_AFTER, CONTENT_LENGTH};
 use hyper_tls::HttpsConnector;
 use crate::message::{WebPushMessage, WebPushService};
 use crate::services::{autopush, firebase};
+use std::future::Future;
 
 /// An async client for sending the notification payload.
 pub struct WebPushClient {
@@ -25,7 +26,7 @@ impl WebPushClient {
     }
 
     /// Sends a notification. Never times out.
-    pub async fn send(&self, message: WebPushMessage) -> Result<(), WebPushError> {
+    pub fn send(&self, message: WebPushMessage) -> impl Future<Output = Result<(), WebPushError>> + 'static {
         let service = message.service.clone();
 
         let request: HttpRequest<Body> = match service {
@@ -35,45 +36,49 @@ impl WebPushClient {
 
         trace!("Request: {:?}", request);
 
-        let response = self.client.request(request).await?;
+        let requesting = self.client.request(request);
 
-        let retry_after = response
-            .headers()
-            .get(RETRY_AFTER)
-            .and_then(|ra| ra.to_str().ok())
-            .and_then(|ra| RetryAfter::from_str(ra));
+        async move {
+            let response = requesting.await?;
 
-        let response_status = response.status();
-        trace!("Response status: {}", response_status);
+            let retry_after = response
+                .headers()
+                .get(RETRY_AFTER)
+                .and_then(|ra| ra.to_str().ok())
+                .and_then(|ra| RetryAfter::from_str(ra));
 
-        let content_length: usize = response
-            .headers()
-            .get(CONTENT_LENGTH)
-            .and_then(|s| s.to_str().ok())
-            .and_then(|s| s.parse().ok())
-            .unwrap_or(0);
+            let response_status = response.status();
+            trace!("Response status: {}", response_status);
 
-        let mut body: Vec<u8> = Vec::with_capacity(content_length);
-        let mut chunks = response.into_body();
+            let content_length: usize = response
+                .headers()
+                .get(CONTENT_LENGTH)
+                .and_then(|s| s.to_str().ok())
+                .and_then(|s| s.parse().ok())
+                .unwrap_or(0);
 
-        while let Some(chunk) = chunks.next().await {
-            body.extend_from_slice(&chunk?);
-        }
-        trace!("Body: {:?}", body);
+            let mut body: Vec<u8> = Vec::with_capacity(content_length);
+            let mut chunks = response.into_body();
 
-        let response = match service {
-            WebPushService::Firebase => {
-                firebase::parse_response(response_status, body.to_vec())
+            while let Some(chunk) = chunks.next().await {
+                body.extend_from_slice(&chunk?);
             }
-            _ => autopush::parse_response(response_status, body.to_vec()),
-        };
+            trace!("Body: {:?}", body);
 
-        debug!("Response: {:?}", response);
+            let response = match service {
+                WebPushService::Firebase => {
+                    firebase::parse_response(response_status, body.to_vec())
+                }
+                _ => autopush::parse_response(response_status, body.to_vec()),
+            };
 
-        if let Err(WebPushError::ServerError(None)) = response {
-            Err(WebPushError::ServerError(retry_after))
-        } else {
-            Ok(response?)
+            debug!("Response: {:?}", response);
+
+            if let Err(WebPushError::ServerError(None)) = response {
+                Err(WebPushError::ServerError(retry_after))
+            } else {
+                Ok(response?)
+            }
         }
     }
 }
