83140f2dc7856a8124508195f4a24e66ff73648c
Julius de Bruijn <julius.debruijn@360dialog.com>
2019-12-17 10:35:04 +0000

    rebuild pages at 6e080f823feb8d7c0f8397dd77f6a27abb259754

---
+++ v0.6.1/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["web_push"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"web_push/enum.WebPushError.html\" title=\"enum web_push::WebPushError\">WebPushError</a>",synthetic:false,types:["web_push::error::WebPushError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
