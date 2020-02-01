2b4007d39233f84d431aab21a3f7e8347c566d10
Julius de Bruijn <julius.debruijn@360dialog.com>
2020-02-01 16:32:40 +0000

    rebuild pages at 1299255645b4dc3cb0abcdd33b11ae85cb3cf1b5

---
+++ v0.7.1/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["web_push"] = {"name":"","dirs":[{"name":"services","files":["autopush.rs","firebase.rs","mod.rs"]},{"name":"vapid","files":["builder.rs","key.rs","mod.rs","signer.rs"]}],"files":["client.rs","error.rs","http_ece.rs","lib.rs","message.rs"]};
+createSourceSidebar();
